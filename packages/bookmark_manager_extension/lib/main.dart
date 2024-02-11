56968013bd7e99cb50316c18a34b583c89058362
Cavin <cavinmacwan1@gmail.com>
2024-02-11 07:45:58 +0530

    Added extension adding functionality

--- packages/bookmark_manager_extension/lib/main.dart
+++ packages/bookmark_manager_extension/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:bookmark_manager_extension/extension_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(const MyApp());
@@ -7,27 +9,11 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
@@ -39,15 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -55,71 +32,196 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+  final controller = Get.put(AddBookmarkController());
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      backgroundColor: const Color(0xFF231651),
+      body: SafeArea(
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                // Padding(
+                //   padding: const EdgeInsets.all(12.0),
+                //   child: Image.asset(
+                //     'assets/images/one_save_background_text.png',
+                //     width: MediaQuery.of(context).size.width * 0.5,
+                //   ),
+                // ),
+                const Spacer(),
+                CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: IconButton(
+                    icon: const Icon(Icons.notifications_active_outlined),
+                    color: Colors.black,
+                    onPressed: () {},
+                  ),
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(16),
+              physics: const NeverScrollableScrollPhysics(),
+              children: [
+                const SizedBox(height: 20),
+                TextField(
+                  controller: controller.titleController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                      hintText: 'Title',
+                      border: OutlineInputBorder(),
+                      hintStyle: TextStyle(color: Colors.white)),
+                ),
+                const SizedBox(height: 8),
+                TextField(
+                  controller: controller.urlController,
+                  style: const TextStyle(color: Colors.white),
+                  decoration: const InputDecoration(
+                      hintText: 'URL',
+                      border: OutlineInputBorder(),
+                      hintStyle: TextStyle(color: Colors.white)),
+                ),
+                const SizedBox(height: 18),
+                GetBuilder<AddBookmarkController>(
+                  builder: (logic) {
+                    return CustomButton(
+                      buttonState: logic.isLoading ? ButtonState.inProgress : ButtonState.normal,
+                      onPressed: () async {
+                        final isAdded = await logic.saveBookmark(
+                          logic.titleController.text,
+                          logic.urlController.text,
+                        );
+                        if (isAdded) {
+                          Get.snackbar('Saved', 'Your bookmark is saved successfully');
+                        } else {
+                          Get.snackbar('Error', 'Your bookmark can\'t be saved',
+                              backgroundColor: Colors.red);
+                        }
+                      },
+                      color: Colors.white,
+                      child: const Text('Save'),
+                    );
+                  },
+                ),
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({
+    required this.onPressed,
+    super.key,
+    this.buttonState = ButtonState.normal,
+    this.progressColor = Colors.white,
+    this.borderRadius,
+    this.child,
+    this.color,
+    this.textColor = Colors.white,
+    this.elevation,
+    this.isDisabled,
+  });
+
+  /// Callback when this button is pressed
+  final Function? onPressed;
+
+  /// State of the button.
+  ///
+  /// Defaults to [ButtonState.normal]
+  final ButtonState buttonState;
+
+  /// Child of the button to show when the
+  /// state is [ButtonState.normal]
+  final Widget? child;
+  final double? borderRadius;
+
+  /// Color for [CircularProgressIndicator] which will be shown
+  /// when state is [ButtonState.inProgress]
+  ///
+  /// Defaults to [Colors.white]
+  final Color progressColor;
+
+  /// Color of the button
+  final Color? color;
+
+  /// Text color
+  final Color? textColor;
+
+  /// Elevation
+  final double? elevation;
+
+  // enable - disable
+  final bool? isDisabled;
+
+  @override
+  Widget build(BuildContext context) {
+    // Get the default radius if it's not done yet
+    var defaultRadius = borderRadius ?? 4;
+
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final height = Theme.of(context).buttonTheme.height;
+        final padding = Theme.of(context).buttonTheme.padding;
+        final width = constraints.maxWidth;
+
+        // When in progress mode, we want to convert the button
+        // to circle so the height and width should be same
+        final widthToSet = buttonState == ButtonState.normal ? width : height;
+
+        // Set the radius to the half of the height in order to
+        // convert the button to circle when in progress mode
+        final radiusToSet = buttonState == ButtonState.normal ? defaultRadius : height / 2;
+
+        // Remove the padding when in the progress mode
+        final paddingToSet = buttonState == ButtonState.normal ? padding : EdgeInsets.zero;
+
+        // Show progress bar when in the progress mode instead
+        // of actual child of this button
+        final childToSet = buttonState == ButtonState.normal
+            ? child
+            : CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(progressColor),
+              );
+
+        return AnimatedContainer(
+          width: widthToSet,
+          height: height,
+          duration: const Duration(
+            milliseconds: 300,
+          ),
+          curve: Curves.linearToEaseOut,
+          child: ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              shadowColor: Colors.transparent,
+              backgroundColor:
+                  !(isDisabled ?? false) ? color ?? const Color(0xFF231651) : Colors.grey,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(radiusToSet),
+              ),
+              padding: paddingToSet,
+              textStyle: TextStyle(color: textColor, fontWeight: FontWeight.w500),
+              elevation: elevation ?? 0,
+            ),
+            onPressed: !(isDisabled ?? false) ? onPressed as void Function()? : null,
+            child: childToSet,
+          ),
+        );
+      },
+    );
+  }
+}
+
+enum ButtonState {
+  /// Indicates the button is showing in progress
+  inProgress,
+
+  /// Indicates the normal state
+  normal,
 }
